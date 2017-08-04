3a6e24500b8d184c0876eed8874c2e16ed41310f
levey <tzp1021@gmail.com>
2017-08-04 13:21:44 +0000

    modify updateOrCreate param

--- database/seeds/TestDataSeeder.php
+++ database/seeds/TestDataSeeder.php
@@ -21,18 +21,18 @@ class TestDataSeeder extends Seeder
     public function run() {
 	$catalogCode = array('100' => '__CATALOG_MUSIC__', '101' => '__CATALOG_RADIO__', '102' => '__CATALOG_TSHOW__');
 	while(list($key, $value) = each($catalogCode)) {
-	    $cata = Catalog::updateOrCreate([
-		'id' => $key,
-		'code' => $value,
+	    $cata = Catalog::updateOrCreate(
+		['id' => $key],
+		['code' => $value,
 		'timeStamp' => time(),
 	    ]);
 	}
 
         $channels = DB::select('select * from whisper_channel_data');
         for($i = 0; $i < count($channels); $i++) {
-	    $chan = Channel::updateOrCreate([
-		'id' => $channels[$i]->channel_md5,
-		'channel_id' => $channels[$i]->channel_id,
+	    $chan = Channel::updateOrCreate(
+		['id' => $channels[$i]->channel_md5],
+		['channel_id' => $channels[$i]->channel_id,
 		'sourceUrl' => $channels[$i]->source_url,
 		'title' => $channels[$i]->title,
 		'iconUrl' => $channels[$i]->icon_url,
@@ -47,9 +47,9 @@ class TestDataSeeder extends Seeder
         $media = DB::select('select * from whisper_media_data');
         for($i = 0; $i < count($media); $i++) {
 #            DB::table('media')->insert([
-	    $me = Media::updateOrCreate([
-                'id' => $media[$i]->media_md5,
-                'videoId' => $media[$i]->youtube_video_id,
+	    $me = Media::updateOrCreate(
+                ['id' => $media[$i]->media_md5],
+                ['videoId' => $media[$i]->youtube_video_id,
                 'netSource' => $media[$i]->source_url,
                 'duration' => $media[$i]->duration,
                 'title' => $media[$i]->title,
@@ -60,16 +60,11 @@ class TestDataSeeder extends Seeder
 
         $channel_media = DB::select('select * from whisper_media_channel_unite_data');
         for($i = 0; $i < count($channel_media); $i++) {
-#	    DB::transaction(function() {
-		$ch_me = ChannelMedia::updateOrCreate([
-        	    'id' => $channel_media[$i]->channel_media_unite_md5,
-                    'channelId' => $channel_media[$i]->channel_md5,
+		$ch_me = ChannelMedia::updateOrCreate(
+        	    ['id' => $channel_media[$i]->channel_media_unite_md5],
+                    ['channelId' => $channel_media[$i]->channel_md5,
 	            'mediaId' => $channel_media[$i]->media_md5,
-                ]);
-#		$ch = Channel::where('id', $channel_media[$i]->channel_md5)->update(['timeStamp', time()]);
-#		$chan = Channel::find($channel_media[$i]->channel_md5);
-#		$ca = Catalog::where('id', $chan->catalogId)->update(['timeStamp', time()]);
-#	    });
+        	]);
         }
 
 	$keywords = array('Vidya Vox', 'Justin Biber', 'Sonu Nigam', 'Ed Sheeran', 'Arijit Singh', 'Mere Rashke Qamar', 'Despacito', 'Mi Gente', 'Qismat', 'Voodoo Song');
