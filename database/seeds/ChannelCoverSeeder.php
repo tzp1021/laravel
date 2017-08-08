c89dd28c3047cb3f711ed8683da7b4cefa0aaa20
levey <tzp1021@gmail.com>
2017-08-08 12:50:29 +0000

    modify pic

--- database/seeds/ChannelCoverSeeder.php
+++ database/seeds/ChannelCoverSeeder.php
@@ -14,7 +14,7 @@ class ChannelCoverSeeder extends Seeder
     public function run()
     {
         $path = 'http://ec2-13-126-95-153.ap-south-1.compute.amazonaws.com/cover/';
-        $names = array('100001.png', '100002.png', '100003.png', '100004.png', '101000.png', '101001.png', '101003.png', '101004.png', '1010006.png', '102000.png', '102002.png', '102003.png', '102008.jpg', '102009.png');
+        $names = array('100001.png', '100002.png', '100003.png', '100004.png', '101000.png', '101001.png', '101003.png', '101004.png', '101006.png', '102000.png', '102002.png', '102003.png', '102008.jpg', '102009.png');
         for($i = 0; $i < count($names); $i++) {
 	    $id = substr($names[$i], 0, 6);
             Channel::where('channel_id', $id)->update(['iconUrl' => $path.$names[$i]]);
