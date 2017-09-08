42cd44efd60de4ba0a3ffe7fc324d6ca9f3249a1
leveyTest <tzp1021@gmail.com>
2017-09-08 13:32:43 +0000

    apk 1.10.8-11

--- database/seeds/UpdateApkVersionInfoSeeder.php
+++ database/seeds/UpdateApkVersionInfoSeeder.php
@@ -13,9 +13,9 @@ class UpdateApkVersionInfoSeeder extends Seeder
     public function run()
     {
         ApkVersion::create([
-    	'versionCode' => 10,
-	    'versionName' => '1.9.6',
-    	'apkUrl' => 'http://feedssource.moment.yirgalab.com/Whisper-webpage-release-1.9.6-10.apk',
+    	'versionCode' => 11,
+	    'versionName' => '1.10.8',
+    	'apkUrl' => 'http://feedssource.moment.yirgalab.com/Whisper-webpage-release-1.10.8-11.apk',
 	    ]);
     }
 }
